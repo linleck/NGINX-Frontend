2f6557cb88482f1df878c1f58fe2d8e709ce363b
linleck <linleckkyalsin26@gmail.com>
2023-07-21 14:00:59 +0630

    簡単なHTMLサイトの作成

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get("/",(req,res)=>{
+res.send("I am a endpoint");
+})
+app.listen(7777, ()=> {
+    console.log("Listening on port 77777");
+})
